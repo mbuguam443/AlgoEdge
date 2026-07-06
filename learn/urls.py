711e469a55bdc19396f593f3a5e85a065697e6d4
Mathew Mbugua <mbuguam443@gmail.com>
2026-07-06 18:02:45 +0300

    Fix enroll URL param: course_slug -> slug to match template and view

--- learn/urls.py
+++ learn/urls.py
@@ -5,6 +5,6 @@ app_name = 'learn'
 urlpatterns = [
     path('', views.CourseListView.as_view(), name='courses'),
     path('<slug:slug>/', views.CourseDetailView.as_view(), name='course_detail'),
-    path('<slug:course_slug>/enroll/', views.enroll_course, name='enroll'),
+    path('<slug:slug>/enroll/', views.enroll_course, name='enroll'),
     path('<slug:course_slug>/lesson/<slug:slug>/', views.LessonDetailView.as_view(), name='lesson_detail'),
 ]
