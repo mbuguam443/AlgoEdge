072745eb2060f8ec46c54053c03d7011cc4dfb64
Mathew Mbugua <mbuguam443@gmail.com>
2026-07-06 13:11:41 +0300

    Remove start_mysql.bat from tracking

--- start_mysql.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "C:\Users\mbugu\OneDrive\Desktop\OpenCode\Trading Services website"
-C:\Users\mbugu\AppData\Local\Programs\Python\Python312\python.exe manage.py runserver 0.0.0.0:8000 --noreload
