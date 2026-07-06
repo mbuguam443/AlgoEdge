6b00d8a8a32322bb70252bab3b09c67d2b8ae3ec
Mathew Mbugua <mbuguam443@gmail.com>
2026-07-06 17:28:31 +0300

    Remove --clear from collectstatic; pre-create staticfiles dir

--- deploy.py
+++ deploy.py
@@ -16,7 +16,9 @@ print("[1/3] Running migrations...")
 call_command('migrate', '--noinput')
 
 print("[2/3] Collecting static files...")
-call_command('collectstatic', '--noinput', '--clear')
+static_dir = os.path.join(BASE_DIR, 'staticfiles')
+os.makedirs(static_dir, exist_ok=True)
+call_command('collectstatic', '--noinput')
 
 print("[3/3] Restarting Passenger...")
 os.makedirs('tmp', exist_ok=True)
